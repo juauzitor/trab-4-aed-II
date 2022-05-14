5564bb4b4749a03f22f039a7541f3dcc6108bcfc
juauzitor <joao.vitor.mendes.pinto@gmail.com>
2022-05-14 10:44:58 -0300

    impressao de todas as palavras inseridas funcionando

--- main.c
+++ main.c
@@ -3,39 +3,37 @@
 #include <string.h>
 #include "ArvTrie2.h"
 
-void gerarstring(tp_no* raiz, char* palavraent){
+void gerarstring(tp_no* raiz, char* palavraent){ // Função para recursivamente ir montando a string da palavra registrada
     tp_no* no = raiz;
     if (no != NULL && no->eh_folha == 1){
-        printf("%s\n", palavraent);
+        printf("%s ", palavraent);
+        for(int i = 0; i < LETRAS; i++){ // For para checar se não existe mais nenhum nó folha depois de outro nó folha
+            if(no->filhos[i] != 0){
+                char copiaent[strlen(palavraent)]; // Stirng auxiliar para evitar mexer diretamente no vetor da substring
+                strcpy(copiaent, palavraent);
+                char straux[2] = {no->filhos[i]->letra, '\0'};
+                strcat(copiaent, straux);
+                gerarstring(no->filhos[i], copiaent);
+            }
+        }
         return;
     }
-    
-    for (int i = 0; i < LETRAS; i++)
-    {
+    for (int i = 0; i < LETRAS; i++){ // For para pecorrer o vetor de filhos buscando não nulos
         if (no->filhos[i] != NULL){
-            char copiaent[strlen(palavraent)+1];
-            for (int i = 0; i < strlen(palavraent); i++)
-            {
-                copiaent[i] = palavraent[i];             
-            }
-            //char straux[2] = {no->filhos[i]->letra, '\0'};
-            //strcat(copiaent, straux);
-            copiaent[strlen(palavraent)] = no->filhos[i]->letra;
-            copiaent[strlen(palavraent)+1] = '\0';
-            //printf("%s\n", palavraent);
+            char copiaent[strlen(palavraent)]; // Stirng auxiliar para evitar mexer diretamente no vetor da substring
+            strcpy(copiaent, palavraent);
+            char straux[2] = {no->filhos[i]->letra, '\0'};
+            strcat(copiaent, straux);
 //            printf("%s\n", copiaent);
-            //gerarstring(no->filhos[i], palavraent);
             gerarstring(no->filhos[i], copiaent);
         }
     }
 }
 
-int imprimirs_no_trie(tp_no* raiz, char* substring)
-{
+int imprimirs_no_trie(tp_no* raiz, char* substring){
     tp_no* no = raiz;
  	int tam = strlen(substring); 
  	int i;
-    
     for(i=0; i < tam; i++)
     {
         int pos = substring[i] - 'a';
@@ -51,18 +49,21 @@ int main (int argc, char *argv[]){
     tp_no * raiz = cria_no(' ');
     char palavraentrada[101];
     while(strcmp(palavraentrada,"FIM")!=0){
-        printf("Digite suas palavras SOUSSA!\n");
-        scanf("%s", palavraentrada); 
+        printf("Digite suas palavras SOUSSA!\nCaso queria sair digite FIM todo em maiúsculo\n");
+        scanf(" %s", palavraentrada);
+    if(strcmp(palavraentrada,"FIM")!=0)
         insere_no_trie(raiz, palavraentrada); //insera a novas palavras na árvore até que seja digitado 'FIM'
     }
     char substring[101];
-    while(1){
+    while(strcmp(substring,"FIM")!=0){
+        printf("caso queria sair digite FIM todo em maiúsculo\n");
         printf("Digite a substring para que seja feito autcomplit!: ");
-        scanf("%s", substring);
-        imprimirs_no_trie(raiz,substring);
-        break;
+        scanf(" %s", substring);
+        if(strcmp(substring,"FIM")!=0){
+            printf("Palavras com %s [ ", substring);
+            imprimirs_no_trie(raiz,substring);
+            printf("]\n\n");
+        }
     }
-    
-    //print_word_in_trie(raiz, "sla");
     return 0;
 }
